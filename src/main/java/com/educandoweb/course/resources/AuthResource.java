c1b883462f0de14389536ec659995b29e0536c95
Alexandrecrp <alexandrecrp202@gmail.com>
2019-10-05 16:29:34 -0300

    Forgot password, Logger

--- src/main/java/com/educandoweb/course/resources/AuthResource.java
+++ src/main/java/com/educandoweb/course/resources/AuthResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.dto.CredentialsDTO;
+import com.educandoweb.course.dto.EmailDTO;
 import com.educandoweb.course.dto.TokenDTO;
 import com.educandoweb.course.services.AuthService;
 
@@ -29,6 +30,12 @@ public class AuthResource {
 	public ResponseEntity<TokenDTO> refresh(){
 		TokenDTO tokenDTO = service.refreshToken();
 		return ResponseEntity.ok().body(tokenDTO);
+	}
+	
+	@PostMapping("/forgot")
+	public ResponseEntity<Void> forgot(@RequestBody EmailDTO dto){
+		service.sendNewPassword(dto.getEmail());
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/educandoweb/course/services/AuthService.java
+++ src/main/java/com/educandoweb/course/services/AuthService.java
@@ -1,5 +1,9 @@
 package com.educandoweb.course.services;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,15 +24,20 @@ import com.educandoweb.course.security.JWTUtil;
 
 import services.exceptions.JWTAuthenticationException;
 import services.exceptions.JWTAuthorizationException;
+import services.exceptions.ResourceNotFoundException;
 
 @Service
 public class AuthService {
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(AuthService.class);
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -72,4 +82,38 @@ public class AuthService {
 		User user = authenticated();
 		return new TokenDTO(user.getEmail(), jwtUtil.generateToken(user.getEmail()));
 	}
+	
+	@Transactional
+	public void sendNewPassword(String email){
+		User user = userRepository.findByEmail(email);
+		if(user == null) {
+			throw new ResourceNotFoundException("Email  not found");
+		}
+		String newPass = newPassword();
+		user.setPassword(passwordEncoder.encode(newPass));
+		
+		userRepository.save(user);
+		LOG.info("New password: " + newPass);
+	}
+	
+	private String newPassword() {
+		char[] vect = new char[10];
+		for (int i=0; i<10; i++) {
+			vect[i] = randomChar();
+		}
+		return new String(vect);
+	}
+	
+	private char randomChar() {
+		Random rand = new Random();
+		int opt = rand.nextInt(3);
+		if(opt == 0) {
+			return (char) (rand.nextInt(10) + 48);
+		} else if (opt ==1) {
+			return (char) (rand.nextInt(26) + 65);
+		} else {
+			return (char) (rand.nextInt(10) + 97);
+		}
+	}
+	
 }
