617f3679b252121f4eee81b77ea3ac781b188fca
Alexandrecrp <alexandrecrp202@gmail.com>
2019-10-05 16:03:22 -0300

    Client accessing own orders

--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -6,10 +6,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.educandoweb.course.dto.OrderDTO;
-import com.educandoweb.course.dto.UserDTO;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.OrderRepository;
@@ -21,6 +19,9 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private AuthService authService;
 
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findAll();
@@ -30,6 +31,13 @@ public class OrderService {
 	public OrderDTO findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
 		Order entity = obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		authService.validadeOwnOrderOrAdmin(entity);
 		return new OrderDTO(entity);
+	}
+	
+	public List<OrderDTO> findByClient(){
+		User client = authService.authenticated();
+		List<Order> list = repository.findByClient(client);
+		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
 }
