13b534aa6908d60c0facec8fbfa4e3412011f3a9
Alexandre <alexandrecrpfeedback@gmail.com>
2019-11-18 13:08:16 -0300

    Order GET endpoints part 1

--- src/main/java/com/educandoweb/course/dto/OrderItemDTO.java
+++ src/main/java/com/educandoweb/course/dto/OrderItemDTO.java
@@ -7,29 +7,33 @@ import com.educandoweb.course.entities.OrderItem;
 public class OrderItemDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
 	private Integer quantity;
 	private Double price;
+	private Long productId;
+	private String productName;
+	private String productImgUrl;
 
-	public OrderItemDTO(Long id, Integer quantity, Double price) {
-		super();
-		this.id = id;
+	public OrderItemDTO() {
+		
+	}
+
+	public OrderItemDTO(Integer quantity, Double price, Long productId, String productName, String productImgUrl) {
 		this.quantity = quantity;
 		this.price = price;
+		this.productId = productId;
+		this.productName = productName;
+		this.productImgUrl = productImgUrl;
 	}
-
-	public OrderItemDTO(OrderItem orderItem) {
-		this.id = orderItem.getOrder().getId();
-		this.quantity = orderItem.getQuantity();
-		this.price = orderItem.getPrice();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
+	
+	public OrderItemDTO(OrderItem entity) {
+		if(entity.getProduct()==null) {
+			throw new IllegalArgumentException("Product was null");
+		}
+		this.quantity = entity.getQuantity();
+		this.price = entity.getPrice();
+		this.productId = entity.getProduct().getId();
+		this.productName = entity.getProduct().getName();
+		this.productImgUrl = entity.getProduct().getImgUrl();
 	}
 
 	public Integer getQuantity() {
@@ -47,5 +51,35 @@ public class OrderItemDTO implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductImgUrl() {
+		return productImgUrl;
+	}
+
+	public void setProductImgUrl(String productImgUrl) {
+		this.productImgUrl = productImgUrl;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 
 }
